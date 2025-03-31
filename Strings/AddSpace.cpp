d9ff783a77a119824ee722d820c359a76a995540
Nitin10cd <nsaxenacse@gmail.com>
2025-03-31 23:18:16 +0530

    adding spaces

---
+++ Strings/AddSpace.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+string addSpaces(string s, vector<int>& spaces) {
+    int m = s.length();
+    int n = spaces.size();
+    string result = "";
+
+    int j = 0;
+    for (int i = 0; i < m; i++) {
+        if (j < n && i == spaces[j]) {
+            result+=" ";
+            j++;
+        }
+        result+=s[i];
+    }
+    return result;
+}
